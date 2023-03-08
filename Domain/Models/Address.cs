0837d134c58d3e7f623a0b1bc4122400b1d9cc2e
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 13:59:19 +0600

    Add Some Domain Class and Microsoft.EntityFrameworkCore.Sql Pakage

---
+++ Domain/Models/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public  class Address : BaseEntity
+    {
+        public string? StreetAdress
+        {
+            get;
+            set;
+        }
+        public string? City
+        {
+            get;
+            set;
+        }
+        public string? State
+        {
+            get;
+            set;
+        }
+        public string? ZipCode
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Domain/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Person : BaseEntity
+    {
+        public string? Name
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get;
+            set;
+        }
+        public Address? Address
+        {
+            get;
+            set;
+        }
+        public List<Email>? Emails
+        {
+            get;
+            set;
+        }
+    }
+}
