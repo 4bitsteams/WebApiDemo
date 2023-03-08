1783ea162a26ce3bb3bdc1808edc17c6d9df7681
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 14:21:27 +0600

    Some Configuration Done

--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -1,3 +1,6 @@
+using DataAccessEF.DbContex;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<PeopleContext>(options => options.UseSqlServer(System.Configuration.GetConnectionString("Default")));
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<PeopleContext>(x => x.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
