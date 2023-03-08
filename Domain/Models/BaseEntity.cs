2391b3a4764b460d7027bbcdeec54c7181f889ab
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 13:52:32 +0600

    Add BaseEntity Class

---
+++ Domain/Models/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models
+{
+    public  class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int UpdatedBy { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public int DeletedBy { get; set; }
+
+        public int DeletedDate { get; set; }
+
+        public Boolean IsActive { get; set; }
+
+        public Boolean IsDeleted { get; set; }
+    }
+}
